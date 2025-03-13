a47a42b4370522a7c9b3e0b5d200f227f7977de8
kanechan25 <kt.itengineer@gmail.com>
2025-03-13 22:23:15 +0700

    add zustand + eslint

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
+import Button from '@/components/Button'
+import { useRouter } from 'next/navigation'
+
 export default function Dashboard() {
+  const router = useRouter()
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Dashboard</h1>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-4xl font-bold'>Dashboard</h1>
+      <Button onClick={() => router.push('/')}>Back to Home</Button>
     </div>
-  );
+  )
 }
