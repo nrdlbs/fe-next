a427b59b7ae1f4d85c137be3faac34fb20ebb300
tiendn <15717476+tiendn@users.noreply.github.com>
2025-04-18 16:02:28 +0700

    feat: integrate Mixpanel for analytics tracking in production

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  env: {
+    NEXT_PUBLIC_MIXPANEL_TOKEN: process.env.NEXT_PUBLIC_MIXPANEL_TOKEN,
+  },
+}
 
 export default nextConfig;
